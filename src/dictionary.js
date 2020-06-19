85f97599cf494208f534b68295d597deea8c086a
Brayan Iribe <brayaniribe@gmail.com>
2020-06-18 23:29:11 -0700

    img fix

--- src/dictionary.js
+++ src/dictionary.js
@@ -11,28 +11,28 @@ const es = {
   steps: [
     {
       id: 0,
-      img: "/img/run.png",
+      img: "img/run.png",
       title: "Paso 1 - Ir al appdata",
       body:
         'En Windows, usa Win+R para abrir la ventana de ejecutar. Aquí, escribe "%appdata%".',
     },
     {
       id: 1,
-      img: "/img/hellogames-folder.png",
+      img: "img/hellogames-folder.png",
       title: "Paso 2 - Hacer backup de la carpeta HelloGames",
       body:
         "Dentro de la carpeta appdata, busca la carpeta HelloGames y haz una copia de ella.",
     },
     {
       id: 2,
-      img: "/img/hellogames-folder-contents.png",
+      img: "img/hellogames-folder-contents.png",
       title: "Paso 3 - Busca tu save data",
       body:
         "Entra a la carpeta HelloGames y posterior a NMS. Aquí habrán muchas carpetas, ordenalas por fecha de modificación y entra a la primera.",
     },
     {
       id: 3,
-      img: "/img/savedata-files.png",
+      img: "img/savedata-files.png",
       title: "Paso 4 - Busca tu archivo save data",
       body:
         "Habrán muchos archivos, los que empiezan con save son tus save datas. Se encuentran ordenados por fecha de modificación, así que toca deducir cual por el último jugado. NO TOQUES ningún otro archivo. Arrastra y sueltalos aquí abajo hasta que encuentres los deseados.",
@@ -53,27 +53,27 @@ const en = {
   steps: [
     {
       id: 0,
-      img: "/img/run.png",
+      img: "img/run.png",
       title: "Step 1 - Go to appdata",
       body: 'In Windows, use Win+R to open Run dialog. Here, type "%appdata%".',
     },
     {
       id: 1,
-      img: "/img/hellogames-folder.png",
+      img: "img/hellogames-folder.png",
       title: "Step 2 - Do backup of HelloGames folder",
       body:
         "Inside the appdata folder, find the HelloGames and do a backup of it.",
     },
     {
       id: 2,
-      img: "/img/hellogames-folder-contents.png",
+      img: "img/hellogames-folder-contents.png",
       title: "Step 3 - Find your save data folder",
       body:
         "Get inside HelloGames and enter to NMS folder, here find your save data. There could be multiple matches, sort them by date to get the desired one.",
     },
     {
       id: 3,
-      img: "/img/savedata-files.png",
+      img: "img/savedata-files.png",
       title: "Step 4 - Find your save data file",
       body:
         "The save files are your save datas. DO NOT TOUCH mf_save or other files here!. Pick the desired save data, they're sorted by modification date.",
